3ad8e6a40737477192d1c538c05a06ded7725473
Nader <na511dr@hotmail.com>
2020-06-16 02:38:08 +0300

    before refactor to use Selenide

--- src/test/java/Base/BaseTests.java
+++ src/test/java/Base/BaseTests.java
@@ -1,5 +1,8 @@
 package Base;
 
+import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,6 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import pages.HomePage;
+
 
 public class BaseTests {
     protected HomePage homePage;
